a0e1429e071c263ce854c0e63ee9d25c141d1d90
Amaury <amaury.catelan@gmail.com>
2017-07-31 20:30:54 -0300

    =Update PHP ENV function in bash script

--- commands.bash
+++ commands.bash
@@ -54,8 +54,55 @@ alias n=n
 
 function p() {
 
-  docker run -it --rm -v $(pwd):/var/www/app -v $A_COMPOSER:$A_USER_HOME/.composer -v $A_CONFIG:$A_USER_HOME/.config -v $A_CACHE:$A_USER_HOME/.cache -v $A_LOCAL:$A_USER_HOME/.local -v $A_SSH:$A_USER_HOME/.ssh ambientum/php:7.1 "$@"
+  # ===========
+  # [ ] need refactoring
+
+  # ===========
+  # set delimiter to split commands
+
+  get="$@"
+  delimiter=",,"
+
+  # ===========
+  # regex with sed
+
+  regex="[^$delimiter]*\($delimiter.*\)"
+  main=$(echo "${get}" | sed "s/$regex/\1/")
+
+  # ===========
+
+  # notes:
+  #
+  # ${#var} - return the lenght. example: 12
+  # (-a) - is boolean operator AND
+  # | xargs - remove whitespaces
+  # eval - run string as the command
+
+  if [ ${#main} -lt ${#get} -a ${#main} -gt 0 ]; then
+
+    main=$(echo "${main}" | cut -c $(( ${#delimiter} + 1 ))- | xargs)
+    itens=$(echo "${get}" | sed "s/$main//" | xargs | rev | cut -c $(( ${#delimiter} + 1 ))- | rev | xargs)
+
+    # ===========
+    # debug
+    #
+    # echo $main
+    # echo $itens
+
+    docker run -it --rm `eval echo $itens` -v $(pwd):/var/www/app -v $A_COMPOSER:$A_USER_HOME/.composer -v $A_CONFIG:$A_USER_HOME/.config -v $A_CACHE:$A_USER_HOME/.cache -v $A_LOCAL:$A_USER_HOME/.local -v $A_SSH:$A_USER_HOME/.ssh ambientum/php:7.1 `eval echo $main`
+
+  else
+
+    # ===========
+    # original script with ports configuration
+
+    docker run -it --rm -p 8001:8000 -v $(pwd):/var/www/app -v $A_COMPOSER:$A_USER_HOME/.composer -v $A_CONFIG:$A_USER_HOME/.config -v $A_CACHE:$A_USER_HOME/.cache -v $A_LOCAL:$A_USER_HOME/.local -v $A_SSH:$A_USER_HOME/.ssh ambientum/php:7.1 "$@"
+
+  fi
 
 }
 
 alias p=p
+
+# ===========
+# ===========
